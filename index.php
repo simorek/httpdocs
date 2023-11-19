032fb885ddc76f3e09cbf35c2c9a3538b72eb929
simorek <143930873+simorek@users.noreply.github.com>
2023-11-19 09:50:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$urlname="test";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $urlname ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+<div class="container">
+  <div class="vertical-center">
+    <h1><?php echo $urlname ?></h1>
+	<p>domain</p>
+  </div>
+</div>
+
+
+
+</body>
+</html>
